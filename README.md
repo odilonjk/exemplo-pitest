97e64fbfb087b8265eb85f61c7ce4072837af0dc
odilonjk <kroger.odilonj@gmail.com>
2018-01-21 13:54:01 -0200

    Base para o exemplo de como utilizar PITest

---
+++ README.md
@@ -0,0 +1,8 @@
+Blog: [krogercode](https://krogercode.wordpress.com/2018/01/21/java-e-os-testes-mutantes/)
+
+Exemplo de como utilizar [PIT Mutation Testing](http://pitest.org/).
+
+* Java: **1.8.0**
+* JUnit: **4.12**
+* PITest: **1.3.1**
+
---
+++ src/test/java/com/wordpress/krogercode/test/VeiculoTest.java
@@ -0,0 +1,62 @@
+package com.wordpress.krogercode.test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.wordpress.krogercode.classe.Veiculo;
+
+public class VeiculoTest {
+	
+	@Test
+	public void testeConstrutor() {
+		
+		String modelo = "Challenger";
+		String marca = "Dodge";
+		int capacidadePortaMalas = 459;
+		
+		Veiculo challenger = new Veiculo(modelo, marca, capacidadePortaMalas);
+		
+		assertTrue(challenger.getMarca().equals(marca));
+		assertTrue(challenger.getModelo().equals(modelo));
+		assertEquals(challenger.getCapacidadeTotalPortaMalas(), capacidadePortaMalas);
+		assertEquals(challenger.getQuantidadeUtilizadaPortaMalas(), 0);
+		assertEquals(challenger.getCapacidadeDisponivelPortaMalas(), capacidadePortaMalas);
+		
+	}
+	
+	@Test
+	public void testeCapacidade() throws Exception {
+		
+		String modelo = "Jimmy";
+		String marca = "Suzuki";
+		int capacidadePortaMalas = 113;
+		
+		Veiculo jimmy = new Veiculo(modelo, marca, capacidadePortaMalas);
+		
+		jimmy.adicionarCargaPortaMalas(13);
+		
+	}
+
+	@Test
+	public void testeValidacaoCapacidade() {
+		
+		String modelo = "Caravan";
+		String marca = "Chevrolet";
+		int capacidadePortaMalas = 774;
+		
+		Veiculo caravan = new Veiculo(modelo, marca, capacidadePortaMalas);
+
+		try {
+			caravan.adicionarCargaPortaMalas(800);
+		} 
+		catch (Exception e) {
+			assertTrue(e.getMessage()
+					.equals("Erro: Excesso de carga. Espaço disponível: " 
+							+ caravan.getCapacidadeDisponivelPortaMalas()));
+		}
+		
+	}
+}
